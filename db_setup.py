66c92c4dfe72bf00e23bfe4e27166d0f09ccf100
Graham Lutz <graham@brainbytescreative.com>
2017-04-26 16:22:55 -0400

    fixes typos in db_setup.py

--- db_setup.py
+++ db_setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -8,29 +8,30 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 class Band(Base):
-	__tabelname__ = 'bands'
+	__tablename__ = 'bands'
 
 	id = Column(Integer, primary_key=True)
 	name = Column(String(250), nullable=False)
 
 class Album(Base):
-	__tabelname__ = 'ablums'
+	__tablename__ = 'albums'
 
 	id = Column(Integer, primary_key=True)
 	name = Column(String(250), nullable=False)
-	band_id = Column(Integer, ForeignKey('band.id'))
+	band_id = Column(Integer, ForeignKey('bands.id'))
 	band = relationship(Band)
 
 class Song(Base):
-	__tabelname__ = 'songs'
+	__tablename__ = 'songs'
 
 	id = Column(Integer, primary_key=True)
 	name = Column(String(250), nullable=False)
 	length = Column(String(8), nullable=False)
 	isHit = Column(Boolean, default=False)
-	band_id = Column(Integer, ForeignKey('band.id'))
+	band_id = Column(Integer, ForeignKey('bands.id'))
 	band = relationship(Band)
-	album_id = Column(Integer, ForeignKey('album.id'))
+	album_id = Column(Integer, ForeignKey('albums.id'))
+	album = relationship(Album)
 
 engine = create_engine('sqlite:///catalogs.db')
 
